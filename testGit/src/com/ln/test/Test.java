8aba38311b6bc3d50719400a970329be73100c56
Allen-WQ <66948371+Allen-WQ@users.noreply.github.com>
2020-08-05 22:31:43 +0800

    Update Test.java

--- testGit/src/com/ln/test/Test.java
+++ testGit/src/com/ln/test/Test.java
@@ -3,7 +3,15 @@ package com.ln.test;
 public class Test {
 
 	private int id;
-
+	private String name;
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
+	public String getName(){
+		return name;
+	} 
 	public int getId() {
 		return id;
 	}
