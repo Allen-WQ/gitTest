3f6494079c79b26b3c88023301c65cbf62cb0031
Allen-WQ <1596231437@qq.com>
2020-08-05 22:42:38 +0800

    testGit2

---
+++ testGit2/src/com/git/test/TestGit.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.git.test;
+
+/**
+ * @author WQL
+ *
+ */
+public class TestGit {
+
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
